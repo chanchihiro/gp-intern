797dae19852beda7b920467c017d8c57e9271a3d
chanchi- <chanchi0707@gmail.com>
2017-02-20 04:37:33 +0900

    js部分の実装(簡単に)

--- src/js/index.js
+++ src/js/index.js
@@ -7,33 +7,31 @@ import Indicator from './components/indicator.js'
 console.log('start'); //スタート
 
 let controller = new ScrollMagic.Controller();
-// let txts = [];
-// txts = document.getElementsByClassName('txt');
-// console.log(txts);
-// let hanasaka = document.getElementById('hanasaka');
-// console.log(hanasaka);
+let box = [];
+box = document.getElementsByClassName('colum');
+console.log(box);
+let box2 = document.getElementsByClassName('passion');
+let firstshow = TweenMax.staggerTo(box, 50, {opacity:1}, 20);
+let second = TweenMax.staggerTo(box2, 50, {opacity:1}, 20);
 
 // let txtshow = TweenMax.staggerTo(txts, 50, {opacity:1, transform:'matrix(1,0,0,1,0,0)'}, 20); //対象,かかる時間,
 // let txtmove = TweenMax.to(hanasaka, 30, {right: "-200%"}, 100);
 
-// let scene = new ScrollMagic.Scene({
-// 	triggerElement: '#story', 
-// 	duration: 80000, 
-// 	triggerHook: 'onLeave'
-// 	})
-// 	.setTween(txtshow)
-// 	.addTo(controller)
-// 	.addIndicators({name: 'txtshow'});
+let scene = new ScrollMagic.Scene({
+	duration: 900, 
+	offset: 300
+	})
+	.setTween(firstshow)
+	.addTo(controller)
+	.addIndicators({name: 'firstshow'});
 
-// let scene2 = new ScrollMagic.Scene({
-// 	triggerElement: '#story',
-// 	duration: 208000,
-// 	triggerHook: 'onLeave'
-// 	})
-// 	.setTween(txtmove)
-// 	.setPin('#story')
-// 	.addTo(controller)
-// 	.addIndicators({name: 'txtmove'});
+let scene2 = new ScrollMagic.Scene({
+	duration: 700,
+	offset: 1000
+	})
+	.setTween(second)
+	.addTo(controller)
+	.addIndicators({name: 'txtmove'});
 
 // $.each(txts, function(i, text) {
 // 	new ScrollMagic.Scene({
